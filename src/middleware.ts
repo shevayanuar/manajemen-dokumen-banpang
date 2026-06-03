dd2034649c63a38bee96cf829c722558a950bae4
Hendry Sheva Yanuar <shevayanuar@proton.me>
2026-06-04 04:11:59 +0700

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
   const token = await getToken({
     req,
     secret: process.env.AUTH_SECRET,
@@ -9,21 +11,19 @@ export async function middleware(req: NextRequest) {
 
   const isLoggedIn = !!token;
 
-  const { pathname } = req.nextUrl;
-
-  if (pathname === "/login" && isLoggedIn) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
+  if (pathname === "/login") {
+    if (isLoggedIn) return NextResponse.redirect(new URL("/dashboard", req.url));
+    return NextResponse.next();
   }
 
-  if (!isLoggedIn && pathname.startsWith("/dashboard")) {
+  if (!isLoggedIn) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  if (
-    pathname.startsWith("/dashboard/pengguna") &&
-    token?.role !== "SUPERADMIN"
-  ) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
+  if (pathname.startsWith("/dashboard/pengguna")) {
+    if (token?.role !== "SUPERADMIN") {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
   }
 
   return NextResponse.next();
